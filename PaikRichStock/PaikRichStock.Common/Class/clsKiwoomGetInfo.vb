eaef8829dfed4b75f7892f422de8c66551d0d195
kellten <32164981+kellten@users.noreply.github.com>
2021-05-12 08:00:59 +0900

    Delete PaikRichStock directory

--- PaikRichStock/PaikRichStock.Common/Class/clsKiwoomGetInfo.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class clsKiwoomGetInfo
-
-End Class
--- PaikRichStock/PaikRichStock/Module/Module1.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Module1
-    Public bLoginStatus As Boolean = False
-    Public 계좌번호 As String = ""
-
-    Public 거래구분목록(13, 2) As String
-    Public 주문유형목록(6, 2) As String
-    Public Const EventOn As Boolean = True
-    Public Const EventOff As Boolean = True
-End Module
