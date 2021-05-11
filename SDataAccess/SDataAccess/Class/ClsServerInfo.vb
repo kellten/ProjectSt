ebb85c2e5a02a42e2946f76e2e4531d3789c76a2
kellten <32164981+kellten@users.noreply.github.com>
2021-05-11 23:35:42 +0900

    기타변경사항

--- SDataAccess/SDataAccess/Class/ClsServerInfo.vb
+++ SDataAccess/SDataAccess/Class/ClsServerInfo.vb
@@ -1,12 +1,12 @@
 ﻿Public Class ClsServerInfo
-    'Public Shared VADISSEVER As String = "EDPB2F011\VADIS"
-    Public Shared VADISSEVER As String = "211.210.61.123, 8081"
+    Public Shared VADISSEVER As String = "EDPB2F011\VADIS"
+    'Public Shared VADISSEVER As String = "211.210.61.123, 8081"
     Public Shared RICHDB As String = "RICHDB"
     Public Shared KIWOOMDB As String = "KIWOOMDB"
 
     Sub New()
-        'VADISSEVER = "EDPB2F011\VADIS"
-        VADISSEVER = "211.210.61.123, 8081"
+        VADISSEVER = "EDPB2F011\VADIS"
+        '   VADISSEVER = "211.210.61.123, 8081"
         RICHDB = "RICHDB"
         KIWOOMDB = "KIWOOMDB"
     End Sub
